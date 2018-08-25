b1108ead1705bb4697d3f61f21bf7f2c9bf23921
Hardy.苏锡强 <alwxkxk@qq.com>
2018-08-25 18:01:10 +0800

    修改pm2脚本

--- hexo/pm2.js
+++ hexo/pm2.js
@@ -1,3 +1,12 @@
-var exec = require('child_process').exec;
-
-exec("hexo server -p 3000",function(){});
+var spawn = require('child_process').spawn;
+/* 其实就是等于执行hexo server --draft*/
+free = spawn('hexo', ['server', '-p 3000']);
+free.stdout.on('data', function (data) {
+  console.log('standard output:\n' + data);
+});
+free.stderr.on('data', function (data) {
+  console.log('standard error output:\n' + data);
+});
+free.on('exit', function (code, signal) {
+  console.log('child process eixt ,exit:' + code);
+});
