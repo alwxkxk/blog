542cdd498e30d9526781f40545414a7ecb3b2076
Hardy.苏锡强 <alwxkxk@qq.com>
2018-08-27 09:24:41 +0800

    添加百度统计

--- hexo/themes/maupassant/source/js/baidu.js
+++ hexo/themes/maupassant/source/js/baidu.js
@@ -1,3 +1,4 @@
+// 百度自动收录
 (function(){
     var bp = document.createElement('script');
     var curProtocol = window.location.protocol.split(':')[0];
@@ -10,3 +11,13 @@
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(bp, s);
 })();
+
+// 百度统计
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?646acd4e6c74eb119c3d5d93c5bfde70";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
