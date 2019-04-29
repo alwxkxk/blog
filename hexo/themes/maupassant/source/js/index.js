6a873023da9e66af77ca90e6ec73d02c80087e1d
alwxkxk <alwxkxk@gmail.com>
2019-04-29 15:51:38 +0800

    修改lazyload代码

--- hexo/themes/maupassant/source/js/index.js
+++ hexo/themes/maupassant/source/js/index.js
@@ -1,15 +1,5 @@
-(function (w, d) {
-  var b = d.getElementsByTagName('body')[0]
-  var s = d.createElement('script')
-  var v = !('IntersectionObserver' in w) ? '8.16.0' : '10.19.0'
-  s.async = true // This includes the script as async. See the "recipes" section for more information about async loading of LazyLoad.
-  s.src = '/js/lazyload.' + v + '.min.js'
-  w.lazyLoadOptions = {/* Your options here */}
-  b.appendChild(s)
-}(window, document))
-
-setTimeout(() => {
+$(document).ready(function(){
   var myLazyLoad = new window.LazyLoad({
     elements_selector: '.lazy'
   })
-}, 2000)
+});
