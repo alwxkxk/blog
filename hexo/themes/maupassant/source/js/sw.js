29e12cf95df6449cd64cc1778596a2178aabeacd
alwxkxk <alwxkxk@gmail.com>
2020-05-23 18:50:03 +0800

    fix: try to fix service not work.

--- hexo/themes/maupassant/source/js/sw.js
+++ hexo/themes/maupassant/source/js/sw.js
@@ -1,5 +1,5 @@
 
-importScripts('/js/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
